76630b68978bca306776bb1cd8ecfd0ae00c1e28
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-02-07 13:14:07 +0100

    Added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>
+        This platform allows us to reach a large and diverse audience who desire to handle common legal issues in the privacy of their own home.</p>
+      <p>
+        Doyourownwill.com was developed to assist those with simple estates create their own will online for free. This website is unique in that it quickly delivers the documents right to the user’s desk through a patent-pending process. Since its inception, Doyourownwill.com has helped millions acquire more information about estate planning and Will preparation and write their Wills.
+
+        Doyourownwill.com has been mentioned in The Los Angeles Times, The New York Post, The Wall Street Journal Sunday edition, and Parenting Magazine – just to name a few!
+      </p>
+    </div>
+  )
+}
+
+export default About
