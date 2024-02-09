abcd103afc27135ea2ea15307e12b079c9afb507
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-02-09 13:15:55 +0100

    Added padding to the contentStyle

--- app/antdtuts.tsx
+++ app/antdtuts.tsx
@@ -24,53 +24,81 @@
 // export default Antdtuts
 
 import React from 'react';
-import { Layout, Flex } from 'antd';
+import { Layout, Flex, Typography, Button } from 'antd';
+import logo from "./images/logo.png"
+import Image from 'next/image';
 
 const { Header, Footer, Sider, Content } = Layout;
 
 const headerStyle: React.CSSProperties = {
     textAlign: 'center',
-    color: '#fff',
-    height: 64,
-    paddingInline: 48,
+    // color: '#ff0000',
+    height: 74,
+    paddingInline: 68,
     lineHeight: '64px',
-    backgroundColor: '#4096ff',
+    // backgroundColor: '#4096ff',
+    backgroundColor: '#FFFFFF',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
 };
 
 const contentStyle: React.CSSProperties = {
     textAlign: 'center',
-    minHeight: 120,
+    // minHeight: 120,
+    padding: '9rem 2rem',
     lineHeight: '120px',
     color: '#fff',
-    backgroundColor: '#0958d9',
+    // backgroundColor: '#0958d9',
+    backgroundColor: '#2F323A'
 };
 
-const siderStyle: React.CSSProperties = {
-    textAlign: 'center',
-    lineHeight: '120px',
-    color: '#fff',
-    backgroundColor: '#1677ff',
-};
+// const siderStyle: React.CSSProperties = {
+//     textAlign: 'center',
+//     lineHeight: '120px',
+//     color: '#fff',
+//     backgroundColor: '#1677ff',
+// };
 
 const footerStyle: React.CSSProperties = {
     textAlign: 'center',
     color: '#fff',
-    backgroundColor: '#4096ff',
+    backgroundColor: '#ffffff'
+    // backgroundColor: '#4096ff',
 };
 
 const layoutStyle = {
-    borderRadius: 8,
+    // borderRadius: 8,
     overflow: 'hidden',
-    width: 'calc(50% - 8px)',
-    maxWidth: 'calc(50% - 8px)',
+    width: 'calc(100% - 8px)',
+    maxWidth: 'calc(100% - 8px)',
 };
 
 const App: React.FC = () => (
     <Flex gap="middle" wrap="wrap">
         <Layout style={layoutStyle}>
-            <Header style={headerStyle}>Header</Header>
-            <Content style={contentStyle}>Content</Content>
-            <Footer style={footerStyle}>Footer</Footer>
+            <Header style={headerStyle}>
+                <div>
+                    <Image src={logo} alt='logo' className='w-48' />
+                </div>
+                <div>
+                    <Typography.Link className='p-4'>Home</Typography.Link>
+                    <Typography.Link className='p-4'>FAQs</Typography.Link>
+                    <Typography.Link className='p-4'>About Us</Typography.Link>
+                    <Typography.Link className='py-2 px-6 bg-blue-400' style={{ color: 'white' }}>GenerateWill</Typography.Link>
+                </div>
+            </Header>
+            <Content style={contentStyle}>
+                <Typography.Title level={1} style={{ color: 'white' }}>
+                    Always the completely free way to make your will.
+                </Typography.Title>
+                <Button>GenerateWill</Button>
+            </Content>
+            <Footer style={footerStyle}>
+                <Typography.Text>
+                    No account needed, no credit card - just 100% free! - Instant download (Word or PDF)
+                </Typography.Text>
+            </Footer>
         </Layout>
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,6 @@ const Home = () => {
       </div>  */}
 
  
-    <h1>Girlll</h1>
     <Antdtuts />
   </div> 
     
