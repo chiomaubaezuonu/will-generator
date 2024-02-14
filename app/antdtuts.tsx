b16574aa8fcbc1a7f4e336da1df03ea87c01013f
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-02-14 10:51:03 +0100

    Changed the favicon

--- app/antdtuts.tsx
+++ app/antdtuts.tsx
@@ -1,38 +1,16 @@
 "use client"
-// import { Button,  DatePicker,  Input, Space, Typography } from 'antd'
-// import React from 'react'
 import "./App.scss"
-// import logo from "./images/logo.png"
-// import Image from 'next/image'
-
-// const Antdtuts = () => {
-//     return (
-//         <div className='app'>
-//             <header className='appHeader'>
-//                 <Space size={12} direction='vertical'>
-//                     <Image src={logo} alt='logo' />
-//                     <Input placeholder='First Name' />
-//                     <Input placeholder='Last Name' />
-//                     <Button  className='ant-btn'>Click me</Button>
-//                     <DatePicker className="datePicker"></DatePicker>
-//                 </Space>
-//             </header>
-//         </div>
-//     )
-// }
-
-// export default Antdtuts
-
 import React from 'react';
-import { Layout, Flex, Typography, Button } from 'antd';
+import { Layout, Flex, Typography, Button, Space, ConfigProvider } from 'antd';
 import logo from "./images/logo.png"
 import Image from 'next/image';
+import Check from "./Check/page";
 
 const { Header, Footer, Sider, Content } = Layout;
 
 const headerStyle: React.CSSProperties = {
     textAlign: 'center',
-    // color: '#ff0000',
+    color: '#777',
     height: 74,
     paddingInline: 68,
     lineHeight: '64px',
@@ -73,8 +51,12 @@ const layoutStyle = {
     width: 'calc(100% - 8px)',
     maxWidth: 'calc(100% - 8px)',
 };
+const handleForm = () => {
+    alert("yes")
+}
 
 const App: React.FC = () => (
+
     <Flex gap="middle" wrap="wrap">
         <Layout style={layoutStyle}>
             <Header style={headerStyle}>
@@ -82,18 +64,19 @@ const App: React.FC = () => (
                     <Image src={logo} alt='logo' className='w-48' />
                 </div>
                 <div>
-                    <Typography.Link className='p-4'>Home</Typography.Link>
-                    <Typography.Link className='p-4'>FAQs</Typography.Link>
-                    <Typography.Link className='p-4'>About Us</Typography.Link>
-                    <Typography.Link className='py-2 px-6 bg-blue-400' style={{ color: 'white' }}>GenerateWill</Typography.Link>
+                    <Typography.Link className='p-4' style={{ color: '#777', borderRight: "0.001rem solid rgba(119, 119,119, 0.1)", }}>Home</Typography.Link>
+                    <Typography.Link href="/faqs" className='p-4' style={{ color: '#777', borderRight: "0.001rem solid rgba(119, 119,119, 0.1)", }}>FAQs</Typography.Link>
+                    <Typography.Link className='p-4' style={{ color: '#777', borderRight: "0.001rem solid rgba(119, 119,119, 0.1)", }}>About Us</Typography.Link>
+                    <Typography.Link className='py-2 px-6 ml-2 bg-blue-400' style={{ color: 'white' }}>GenerateWill</Typography.Link>
                 </div>
             </Header>
             <Content style={contentStyle}>
                 <Typography.Title level={1} className="pageContentText">
                     Always the completely free way <br /> to make your will.
                 </Typography.Title>
-                <Button style={{textAlign:"center"}}>GenerateWill</Button>
+                <Button type="primary" textHoverBg onClick={handleForm}>GenerateWill</Button>
             </Content>
+            <Check/>
             <Footer style={footerStyle}>
                 <Typography.Text className='text-xl font-bold'>
                     No account needed, no credit card - just 100% free! - Instant download (Word or PDF)
@@ -104,5 +87,5 @@ const App: React.FC = () => (
 
     </Flex>
 );
-
 export default App;
+
--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -1,47 +1,34 @@
-import React from 'react'
+"use client"
+import { Card, Typography } from 'antd';
+const Faqs: React.FC = () => (
+    <div className='mt-16 flex flex-col  items-center'>
+        <h1 className=' mb-6 font-bold text-2xl ml-0'>FAQs</h1>
+        <Card className='w-1/2 mb-4 '>
+            <p className='text-[#2f323a] text-lg font-bold'> Do I have to share my email address to complete my Will?</p>
+            <p className='card-text'>No. You will not be asked to provide an email address to complete or save your Will.</p>
+            <p className='card-text'> DoYourOwnWill.com does not collect or sell email addresses.</p>
+        </Card>
+        <Card className='w-1/2 mb-4 '>
+            <p className='text-[#2f323a] text-lg font-bold'>   Is it really free?</p>
+            <p className='card-text'>Yes. DoYourOwnWill.com is 100% free and doesn't accept payments anywhere on <br /> the site, you'll never have to pay for a document you make or download.</p>
+            {/* <p className='card-text'> DoYourOwnWill.com does not collect or sell email addresses.</p> */}
+        </Card>
+        <Card className='w-1/2 mb-4 '>
+            <p className='text-[#2f323a] text-lg font-bold'>Do I need to create an account?</p>
+            <p className='card-text'>No. You don’t need to create an account to download any document on this site.</p>
+        </Card>
+        <Card className='w-1/2 mb-4 '>
+            <p className='text-[#2f323a] text-lg font-bold'>Why should I write a Will?</p>
+            <p className='card-text'>Writing a Will is critically important for all adults regardless of wealth, marital status, or age. A Will <br /> allows you to: ensure that your possessions will be distributed as you wish; appoint and outline powers <br /> of an executor and/or trustee; appoint a guardian for minor children; specify funeral wishes; expedite <br /> the legal process; and reduce stress and heartache for loved ones.</p>
+        </Card>
+        <Card className='w-1/2 mb-4 '>
+            <p className='text-[#2f323a] text-lg font-bold'>What happens after signing my Will</p>
+            <p className='card-text'> After signing the Will:
+                Decide where the original Will is to be kept and inform the executor of the location. and deliver it (getting a receipt). Beware of storage in a bank safety deposit box as some states require that they be sealed upon death and retrieval of the Will documents could be difficult. Generally, storage in a fireproof box or some other safe location should be sufficient.</p>
+                <br />
+            <p>Be sure to let the executor and any alternates know the location of the Will. Remember that, should you desire to change your Will in the future, a formal Codicil or new Will will have to be executed. You should not write on or otherwise attempt to revise the Will.</p>
+        </Card>
+    </div>
+);
 
-const Faqs = () => {
-    return (
-        <div>
-            <div>
-                <h2> Do I have to share my email address to complete my Will?</h2>
-                <p>No. You will not be asked to provide an email address to complete or save your Will. DoYourOwnWill.com does not collect or sell email addresses.</p>
-            </div>
-            <div>
-                <h2>
-                    Is it really free?
-                </h2>
-                <p>
-                    DoYourOwnWill.com collects revenue by placing relevant advertisements along the bottom and side of the web page and through affiliate relationships with other companies.
-
-                    Your sensitive information is never showModal
-                </p>
-            </div>
-            <div>
-                <h2>Do I need to create an account?</h2>
-                <p>
-                    No. You don’t need to create an account to download any document on this site.
-                </p>
-            </div>
-            <div>
-                <h2>Why should I write a Will?</h2>
-                <p>
-                    Writing a Will is critically important for all adults regardless of wealth, marital status, or age. A Will allows you to: ensure that your possessions will be distributed as you wish; appoint and outline powers of an executor and/or trustee; appoint a guardian for minor children; specify funeral wishes; expedite the legal process; and reduce stress and heartache for loved ones. See the DYOW Blog or Why Do I Need a Will page of this site for more information.
-                </p>
-            </div>
-            <div>
-                <h2>What happens after signing my Will?</h2>
-                <p>
-                    After signing the Will:
-                    Decide where the original Will is to be kept and inform the executor of the location. and deliver it (getting a receipt). Beware of storage in a bank safety deposit box as some states require that they be sealed upon death and retrieval of the Will documents could be difficult. Generally, storage in a fireproof box or some other safe location should be sufficient.
-
-                    Be sure to let the executor and any alternates know the location of the Will. Remember that, should you desire to change your Will in the future, a formal Codicil or new Will will have to be executed. You should not write on or otherwise attempt to revise the Will.
-
-                    Remember: Periodically review changes in your life and consider whether you should change your Will.
-                </p>
-            </div>
-        </div>
-    )
-}
-
-export default Faqs
+export default Faqs;
